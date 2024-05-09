5e8c5d398fed28da99c873888467cf05aa879410
YvetteCarlos <89424585+YvetteCarlos@users.noreply.github.com>
2024-05-08 23:28:19 -0700

    Added user_status.py
    some changes need to be made in user and main to run this

---
+++ user_status.py
@@ -0,0 +1,44 @@
+import psycopg2
+import db_connection as db
+
+
+def user_status(username, change_status):
+    try:
+        conn = db.connectDataBase()
+        if conn is None:
+            print("Failed to connect to the database.")
+            return False
+        cur = conn.cursor()
+        status_sql = "UPDATE \"usersIP\" SET status = %s WHERE username = %s"
+        cur.execute(status_sql, (change_status, username,)) #change status to true 
+        conn.commit()
+        print("User is now ONLINE")
+        return True
+    except psycopg2.Error as e:
+        print("Could not update Status:", e)
+        return False
+    finally:
+        if conn is not None:
+            conn.close()
+
+def not_logged_in(username, change_status):
+    try:
+        conn = db.connectDataBase()
+        if conn is None:
+            print("Failed to connect to the database.")
+            return False
+    
+        cur = conn.cursor()
+        status_sql = "UPDATE \"usersIP\" SET status = %s WHERE username = %s"
+        cur.execute(status_sql, (change_status,username,)) #change status to False
+        conn.commit()
+        print("User is now OFFLINE")
+        return False
+    except Exception as e:
+        print("An error occurred:", e)
+        return False
+    finally:
+        if conn:
+            conn.close()
+
+
