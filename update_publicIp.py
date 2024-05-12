ed2de14369303219f31c3f02cdece4b1ce2b7089
Youstina Gerges <102266055+yngerges-pro@users.noreply.github.com>
2024-05-11 23:31:30 -0700

    Fixed update_publicIp.py

--- update_publicIp.py
+++ update_publicIp.py
@@ -1,7 +1,17 @@
 import psycopg2
 import db_connection as db
 
-def update_publicIp(username, public_Ip):
+def insert(username, passsword, IP,Port, Status):
+    conn = db.connectDataBase()
+    sql = "INSERT INTO users (username,password,ip,port,status) VALUES (%s,%s,%s,%s,%s)"
+    if conn is None:
+            print("Failed to connect to the database.")
+            return False
+    cur = conn.cursor()
+    cur.execute(sql, (username, passsword, IP, Port, Status))
+    print("Inserted everthing!")
+
+def update_publicIp(username, public_Ip,port, status):
     try:
         conn = db.connectDataBase()
         if conn is None:
@@ -9,8 +19,8 @@ def update_publicIp(username, public_Ip):
             return False
 
         cur = conn.cursor()
-        update_sql = "UPDATE \"usersIP\" SET ip = %s WHERE username = %s"
-        cur.execute(update_sql, (public_Ip, username))
+        update_sql = "UPDATE \"users\" SET ip = %s, status = %s, port =%s WHERE username = %s"  
+        cur.execute(update_sql, (public_Ip, status, port, username))
         conn.commit()
         return True
     except psycopg2.Error as e:
@@ -20,7 +30,7 @@ def update_publicIp(username, public_Ip):
         if conn is not None:
             conn.close()
 
-def check_public_ip(username, current_ip):
+def check_public_ip(username, current_ip, port, status):
     try:
         conn = db.connectDataBase()
         if conn is None:
@@ -28,13 +38,15 @@ def check_public_ip(username, current_ip):
             return False
 
         cur = conn.cursor()
-        select_sql = "SELECT ip FROM \"usersIP\" WHERE username = %s"
+        #select_sql = "SELECT usersip.ip, users.username FROM usersip INNER JOIN users ON users.username = %s"
+        select_sql = "SELECT ip FROM \"users\" WHERE username = %s"
+
         cur.execute(select_sql, (username,))
         user_ip = cur.fetchone()
 
         if user_ip is not None:
             if user_ip['ip'] != current_ip:
-                if update_publicIp(username, current_ip):
+                if update_publicIp(username, current_ip, port, status):
                     print("Public IP's do not Match!")
                     print("Updated user's public IP!")
                 else:
@@ -49,3 +61,4 @@ def check_public_ip(username, current_ip):
     finally:
         if conn is not None:
             conn.close()
+
