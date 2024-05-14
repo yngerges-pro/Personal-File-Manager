2de8dd395da96a559b471e972f2e21760074d9d8
Abraham Medina <92947123+a-medina-2000@users.noreply.github.com>
2024-05-13 19:02:18 -0700

    Update DownloadFileMethods.py

--- DownloadFileMethods.py
+++ DownloadFileMethods.py
@@ -1,39 +1,54 @@
 # Methods used for the Download Files Screen
+import db_connection as db
 from ReceivingSide import *
 
-def viewAllDownloadableFiles(cursorObject):
-    sql = ""
-    # Need to join files table and usersip table together, only return where
-    # Status = true in usersip
+def viewAllDownloadableFiles():
+    conn = db.connectDataBase()
+    curr = conn.cursor()
 
-    # Could just call the search method where search by ""
+    # Need to join files table and users tables, only return where
+    # Status = true in usersip
+    sql = 'SELECT "FileName", "FileSize", "Description" FROM files JOIN users ON users.status = true'
+
+    result = curr.execute(sql)
+
+    conn.close()
+    curr.close()
 
     # result is a list
     return result
 
-def searchForDownloadableFiles(searchWith, cursorObject):
-    searchWith = "%" + searchWith + "%" # Add wild cards front and back of searching with
-    sql = ""
+def searchForDownloadableFiles(searchWith):
+    conn = db.connectDataBase()
+    curr = conn.cursor()
 
+    searchWith = "%" + searchWith + "%" # Add wild cards front and back of searching with
+    sql = "SELECT f.FileName, f.FileSize, f.Description FROM files JOIN users ON u.status = true AND fileName = " + searchWith
+
+    result = curr.execute(sql)
+
+    conn.close()
+    curr.close()
     # Need to join files table and usersip table together, only return where
     # Status = true AND LIKE fileName = searchWith
-
     # result is a list
     return result
 
-def downloadThisFile (fileOwnerID, fileName, Path, cursorObject):
-    sql = ""
+def downloadThisFile (fileName, downloadPath):
     # SQL query usersip table, grab IP and Port
+    conn = db.connectDataBase()
+    curr = conn.cursor()
 
-    # Could join usersip and files, and grab IP and Port using fileName
+    sql = "SELECT ip, port FROM users"
+    info = curr.execute(sql)
+    print(info)
+    ip = ""
+    port = 0000
 
-    Ip = ""
-    Port = 0000
-
-    result = downloadFile(Ip, Port, fileName, Path)
+    # result = downloadFile(ip, port, fileName, downloadPath)
 
     # Result is a code
     # 1 = Successful
     # -1 = Error in Connection (Cannot Connect, or Connection Full, or User Is Offline?)
     # -2 = File Wasn't Found (File Owner May Have Moved The File?)
-    return result
+    return info
