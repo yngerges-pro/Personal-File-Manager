8817fd0717cdb4d44f6d83e46e58806274ffc9ad
Abraham Medina <92947123+a-medina-2000@users.noreply.github.com>
2024-05-13 19:03:20 -0700

    Update ShareFileMethods.py

--- ShareFileMethods.py
+++ ShareFileMethods.py
@@ -1,5 +1,7 @@
+import os
 import psycopg2
 import db_connection as db
+import delete_file
 # Methods used for Share Files Screen
 
 def viewMyShareFiles(userID):
@@ -16,10 +18,10 @@ def viewMyShareFiles(userID):
     
     for row in rows:
         file = {
-            'UserID': row['userid'],
-            'FileName': row['filename'],
-            'FileSize': row['filesize'],
-            'Description': row['description']
+            'UserID': row['UserID'],
+            'FileName': row['FileName'],
+            'FileSize': row['FileSize'],
+            'Description': row['Description']
         }
         result.append(file)
 
@@ -28,56 +30,107 @@ def viewMyShareFiles(userID):
     
     return result
 
-def addNewShareFile(userID, fileName, Description, Path):
+def addNewShareFile(userID, fileName, fileSize, description):
     conn = db.connectDataBase()
     cur = conn.cursor()
     # Ensure file is not already being shared
     # Either SQL Query for that file, or check keyword.txt
+    userID = str(userID)
+    description = str(description)
+
+    path = createPath()
+    openKeyword = path + "\\keyword.txt"
+
+    with open(openKeyword, 'r+') as file:
+        allFiles = file.readlines()
+        file.seek(0)
+        file.truncate()
+        
+        if fileName in allFiles:
+            result = -1
 
     # Ensure File Exists in Directory
-
     # If it exists, Add FileName into keyword.txt
+    if os.path.exists(fileName):
+        with open(openKeyword, 'w') as file:
+            file.writelines(fileName)
+            result = 1
+    else:
+        result = -2
 
     # SQL INSERT new row into files table
-    sql = ""
+    sql = 'INSERT INTO files (UserID, FileName, FileSize, Description) VALUES (%s, %s, %s, %s)', (userID, fileName, fileSize, description)
+    cur.execute(sql)
 
-    
     cur.close()
     conn.close()
-    
     # result is a code here, either successful, or not
     # 1 = success
     # -1 = File Already Shared
     # -2 = File Not In Directory
-    
     return result
 
-def editShareFileDescription(userID, fileName, newDescription):
+def editShareFile(userID, fileName, fileSize, description):
     conn = db.connectDataBase()
     cur = conn.cursor()
     # Simply SQL UPDATE the row where userID + fileName, update description
-    sql = ""
+    userID = str(userID)
+
+    fileName = str(fileName)
+    fileNameAdjusted = "'{}'".format(fileName)
+
+    newDescription = input("Enter new description: ")
+    newDescriptionAdjusted = "'{}'".format(newDescription)
+
+    # stop gap, if result's -1 at end, error occurred
+    result = -1
+
+    sql = 'UPDATE files SET "Description" = ' + newDescriptionAdjusted + ' WHERE "UserID" = ' + userID + ' AND "FileName" = ' + fileNameAdjusted
+    cur.execute(sql)
+
+    result = 1
 
     cur.close()
     conn.close()
-    
     # result is a code here, either successful, or not
     # 1 = success
     # -1 = Error
     return result
 
-def deleteShareFile(userID, fileName, Path):
+def deleteShareFile(userID, fileName):
     conn = db.connectDataBase()
     cur = conn.cursor()
     # Simply SQL DELETE row where userID + fileName
-    sql = ""
+    userID = str(userID)
+
+    fileName = str(fileName)
+    fileNameAdjusted = "'{}'".format(fileName)
+
+    sql = 'DELETE FROM files WHERE "UserID" = ' + userID + ' AND "FileName" = ' + fileNameAdjusted
+    cur.execute(sql)
+    result = -1
+
+    path = createPath()
+    openKeyword = path + "\\keyword.txt"
 
     # Delete line in keyword.txt of fileName
+    with open(openKeyword, 'r+') as file:
+        allFiles = file.readlines()
+        file.seek(0)
+        file.truncate()
 
-    cur.close()
-    conn.close()
-    
+        for line in allFiles:
+            if line.strip("\n") != fileName:
+                file.write(line)
+                result = 1   
+
+     
+    delete_file(userID, fileName)
     # result is a code here, either successful, or not
     # 1 = success
     # -1 Error
     return result
+
+def createPath():
+    path = ("Enter main file path: ")
+    return path
